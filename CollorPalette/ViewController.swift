25a95b8d6457b4d62621e7181cc364593beb2aeb
howard <Busta@live.ru>
2023-02-07 22:15:20 +0700

    setting up the sliders

--- CollorPalette/ViewController.swift
+++ CollorPalette/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redLabel: UILabel!
+    @IBOutlet var greenLabel: UILabel!
+    @IBOutlet var blueLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 20
+        
     }
 
-
+    @IBAction func redSliderAction() {
+        redLabel.text = redSlider.value.formatted()
+        changeColor()
+    }
+    @IBAction func greenSliderAction() {
+        greenLabel.text = greenSlider.value.formatted()
+        changeColor()
+    }
+    
+    @IBAction func blueSliderAction() {
+        blueLabel.text = blueSlider.value.formatted()
+        changeColor()
+    }
+    
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
 }
 
